080fb2d15c45fd555de4cfaccdf823894483a0a3
EleLee <610038187@qq.com>
2019-05-22 17:42:26 +0800

    修改文件路径

--- init/router.js
+++ init/router.js
@@ -1,8 +1,10 @@
-const routerConfig = require('../routersConfig/index.js')
 const Router = require('koa-router')
+const fs = require('fs')
+const path = require('path')
+
 const router = Router()
 
-Object.entries(routerConfig).map(([key, value]) => {
+Object.entries(getRoutes()).map(([key, value]) => {
     let routerItem = new Router()
     setRoute(routerItem, value)
     router.use(`/${key}`, routerItem.routes(), routerItem.allowedMethods())
@@ -14,5 +16,26 @@ function setRoute(routerItem, {type = 'get', path, cb, children}, parentPath) {
     routerItem[type](path, async (ctx) => cb(ctx))
     if(children instanceof Array) {
         children.map((child) => setRoute(routerItem, child, path))
+    }
+}
+
+function getRoutes() {
+    const PATH = path.join(__dirname, '../routes')
+    const fileList = fs.readdirSync(PATH)
+    const configObj = {}
+    fileList.map(file => {
+        if(filterFile(file)) return
+        configObj[getName(file)] = require(`${PATH}/${file}`)
+    })
+    return configObj
+
+
+    function filterFile (filename) {
+        const pattern = /i|Index.js$/
+        return pattern.test(filename)
+    }
+
+    function getName (name) {
+        return name.substring(0, name.lastIndexOf('.'))
     }
 }
